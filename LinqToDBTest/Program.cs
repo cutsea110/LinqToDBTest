81f697b6efd739eae6df18d32163c5059cc28e4a
cutsea110 <cutsea110@gmail.com>
2019-06-16 06:08:13 +0900

    データモデルの生成とデータアクセス

--- LinqToDBTest/Program.cs
+++ LinqToDBTest/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DataModels;
+
 namespace LinqToDBTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var db = new AdventureWorks2016DB())
+            {
+                var q =
+                    from c in db.Customers
+                    select new
+                    {
+                        CustomerName = $"{c.Person.FirstName} {c.Person.MiddleName} {c.Person.LastName}",
+                        OrderCount = c.SalesOrderHeaderCustomerIds.Count(),
+                    };
+
+                foreach (var c in q)
+                    Console.WriteLine(c);
+            }
         }
     }
 }
